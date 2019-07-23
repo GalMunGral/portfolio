5a134d3ea499d6d052d1ab9b81fc031fc2898409
Wenqi He <hewenqi96@gmail.com>
2019-07-22 21:11:33 -0400

    Update

---
+++ init.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/GalMunGral/cs4400-marta-traffic.git
+(cd cs4400-marta-traffic/backend && npm install && npm start &)
+(cd cs4400-marta-traffic/frontend && npm install && npm start &)
